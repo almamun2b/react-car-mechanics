55a25a164c445ada8fb3643ed716151dc564ead9
mycodinglab <almamun2b@gmail.com>
2021-10-27 22:44:39 +0600

    Heroky, Rest API Added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -8,7 +8,7 @@ const AddService = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         console.log(data);
-        axios.post('http://localhost:5000/services', data)
+        axios.post('https://pure-cliffs-43947.herokuapp.com/services', data)
             .then(res => {
                 console.log(res);
                 if (res.data.insertedId) {
